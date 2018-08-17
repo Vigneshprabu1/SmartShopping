a9fc59a6bbc98a69bf79268ae74ffcfde231f74e
vigneshprabu <vigneshprabu479@gmail.com>
2018-08-17 12:56:42 +0530

    initial commit

---
+++ src/app/combine.ts
@@ -0,0 +1,22 @@
+export interface Combine {
+    id ?: number;
+    img ?: string;
+    name ?: string;
+    price ?: number;
+    quantity?: string;
+    prodCategory?: String;
+    shopCategory?:String;
+    shortDesc?:String;
+    longDesc?:String;
+    size?:number;
+    rating?:number;
+    discount?:number;
+    brandName?:String;
+    sku?:number;
+    tax?:number;
+    prodId?:number;
+    total?:number;
+    shopName?:string;
+    lat?:number;
+    lng?:number;
+}
---
+++ src/app/product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+    id : number;
+    img : string;
+    name : string;
+    price : number;
+    quantity: string;
+    prodCategory: String;
+    shopCategory:String;
+    shopName:String;
+    shortDesc:String;
+    longDesc:String;
+    size:number;
+    rating:number;
+    discount:number;
+    brandName:String;
+    sku:number;
+    tax:number;
+    prodId:number;
+    total:number;
+
+}
